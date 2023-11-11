cbf3ce5dd9a08df1466ef30c0e366c77ed25ab51
Ty Todd <swagginty@gmail.com>
2023-11-11 02:07:07 -0500

    app store

--- src/components/waitlist.jsx
+++ src/components/waitlist.jsx
@@ -77,7 +77,7 @@ const Waitlist = () => {
               </div>
               <div class="form-group">
                 <label className="form-label2">
-                  Email (must be the one you use for icloud)
+                  Email (must be the one you use for the App Store)
                 </label>
                 <input
                   value={email}
