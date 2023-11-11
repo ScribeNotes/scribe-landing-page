7a4fd25968305fd7586725cd090b0fae753aa9e4
Ty Todd <swagginty@gmail.com>
2023-11-11 00:44:47 -0500

    added pricacy policy

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { Waitlist } from "./components/waitlist";
 import JsonData from "./data/data.json";
 import SmoothScroll from "smooth-scroll";
 import "./App.css";
+import PrivacyPolicy from "./PrivacyPolicyPage.js";
 
 export const scroll = new SmoothScroll('a[href*="#"]', {
   speed: 1000,
@@ -15,10 +16,45 @@ export const scroll = new SmoothScroll('a[href*="#"]', {
 
 const App = () => {
   const [landingPageData, setLandingPageData] = useState({});
+  const [currentPage, setCurrentPage] = useState("home");
+
   useEffect(() => {
     setLandingPageData(JsonData);
   }, []);
 
+  useEffect(() => {
+    // Extract the current page from the URL
+    const path = window.location.pathname;
+    setCurrentPage(path === "/privacypolicy" ? "privacypolicy" : "home");
+  }, []);
+
+  const navigateTo = (page) => {
+    setCurrentPage(page);
+    // Update the URL based on the chosen page
+    window.history.pushState({}, page, `/${page}`);
+  };
+
+  return (
+    <div>
+      {/* <nav>
+        <button onClick={() => navigateTo("home")}>Home</button>
+        <button onClick={() => navigateTo("privacypolicy")}>
+          PrivacyPolicy
+        </button>
+      </nav> */}
+      <div>
+        {currentPage === "home" && <Home />}
+        {currentPage === "privacypolicy" && <PrivacyPolicy />}
+      </div>
+    </div>
+  );
+};
+
+const Home = () => {
+  const [landingPageData, setLandingPageData] = useState({});
+  useEffect(() => {
+    setLandingPageData(JsonData);
+  }, []);
   return (
     <div>
       <Navigation />
@@ -35,5 +71,22 @@ const App = () => {
     </div>
   );
 };
+
+//   return (
+//       <div>
+//         <Navigation />
+//         <Header data={landingPageData.Header} />
+//         {/* <Features data={landingPageData.Features} /> */}
+//         {/* <div id="waitlist" className="mt-20" /> */}
+//         <Waitlist data={landingPageData.Features} />
+//         <About data={landingPageData.About} />
+//         {/* <Services data={landingPageData.Services} /> */}
+//         {/* <Gallery data={landingPageData.Gallery} /> */}
+//         {/* <Testimonials data={landingPageData.Testimonials} /> */}
+//         {/* <Team data={landingPageData.Team} /> */}
+//         <Contact data={landingPageData.Contact} />
+//       </div>
+//   );
+// };
 
 export default App;
